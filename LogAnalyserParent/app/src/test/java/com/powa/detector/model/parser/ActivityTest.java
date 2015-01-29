b76135f7351bfa4da4204a144eaacef2587734f7
Fabio Serragnoli <serragnoli@gmail.com>
2015-01-29 21:43:28 +0000

    Added one more test to cover the second branch of isFailure()

--- LogAnalyserParent/app/src/test/java/com/powa/detector/model/parser/ActivityTest.java
+++ LogAnalyserParent/app/src/test/java/com/powa/detector/model/parser/ActivityTest.java
@@ -15,66 +15,74 @@ public class ActivityTest {
 	private static final String EPOCH = "1420113600";
 	private static final String UPPER_BOUNDARY_OF_SUSPICION_ZONE = "1420113900";
 	private static final String ONE_SECOND_AFTER_SUSPICION_ZONE = "1420113901";
-	private static final String ACTION = "FAILURE";
+	private static final String FAILURE = "FAILURE";
+	private static final String SUCCESS = "SUCCESS";
 	private static final String USERNAME = "John.Doe";
 	
-	private Activity activity;
+	private Activity failedActivity;
 	
 	@Before public void 
 	setup() {
-		activity = new Activity(IP, EPOCH, ACTION, USERNAME);
+		failedActivity = new Activity(IP, EPOCH, FAILURE, USERNAME);
 	}
 	
 	@Test public void 
 	should_record_the_ip() {
-		assertThat(activity.ip(), is(IP));
+		assertThat(failedActivity.ip(), is(IP));
 	}
 	
 	@Test public void 
 	should_record_the_epoch() {
-		assertThat(activity.date(), is(Long.valueOf(EPOCH)));
+		assertThat(failedActivity.date(), is(Long.valueOf(EPOCH)));
 	}
 	
 	@Test public void 
 	should_record_the_action() {
-		assertThat(activity.action(), is(Activity.Action.FAILURE));
+		assertThat(failedActivity.action(), is(Activity.Action.FAILURE));
 	}
 	
 	@Test public void 
 	should_record_the_username() {
-		assertThat(activity.username(), is(USERNAME));
+		assertThat(failedActivity.username(), is(USERNAME));
 	}
 	
 	@Test public void 
 	should_return_true_when_activity_is_failure() {
-		assertThat(activity.isFailure(), is(true));
+		assertThat(failedActivity.isFailure(), is(true));
+	}
+	
+	@Test public void 
+	should_return_false_when_activity_is_success() {
+		Activity successActivity = new Activity(IP, EPOCH, SUCCESS, USERNAME);
+		
+		assertThat(successActivity.isFailure(), is(false));
 	}
 	
 	@Test public void 
 	should_arise_suspicion_when_both_activities_have_the_same_epoch() {
-		Activity sameEpoch = new Activity(IP, EPOCH, ACTION, USERNAME);
+		Activity sameEpoch = new Activity(IP, EPOCH, FAILURE, USERNAME);
 		
-		assertThat(sameEpoch.withinSuspiciousRange(activity), is(SUSPICIOUS));
+		assertThat(sameEpoch.withinSuspiciousRange(failedActivity), is(SUSPICIOUS));
 	}
 	
 	@Test public void 
 	should_arise_suspicion_as_it_is_exactly_five_minutes_apart_from_each_other() {
-		Activity exactlyFiveMinsLater = new Activity(IP, UPPER_BOUNDARY_OF_SUSPICION_ZONE, ACTION, USERNAME);
+		Activity exactlyFiveMinsLater = new Activity(IP, UPPER_BOUNDARY_OF_SUSPICION_ZONE, FAILURE, USERNAME);
 		
-		assertThat(exactlyFiveMinsLater.withinSuspiciousRange(activity), is(SUSPICIOUS));
+		assertThat(exactlyFiveMinsLater.withinSuspiciousRange(failedActivity), is(SUSPICIOUS));
 	}
 	
 	@Test public void 
 	should_arise_suspicion_as_it_is_exactly_five_minutes_apart_from_each_other_inverted() {
-		Activity exactlyFiveMinsLater = new Activity(IP, UPPER_BOUNDARY_OF_SUSPICION_ZONE, ACTION, USERNAME);
+		Activity exactlyFiveMinsLater = new Activity(IP, UPPER_BOUNDARY_OF_SUSPICION_ZONE, FAILURE, USERNAME);
 		
-		assertThat(activity.withinSuspiciousRange(exactlyFiveMinsLater), is(SUSPICIOUS));		
+		assertThat(failedActivity.withinSuspiciousRange(exactlyFiveMinsLater), is(SUSPICIOUS));		
 	}
 	
 	@Test public void 
 	should_be_normal_if_activity_one_second_greater_than_five_minutes_apart_from_each_other() {
-		Activity oneSecondBeforeSuspicionZone = new Activity(IP, ONE_SECOND_AFTER_SUSPICION_ZONE, ACTION, USERNAME);
+		Activity oneSecondBeforeSuspicionZone = new Activity(IP, ONE_SECOND_AFTER_SUSPICION_ZONE, FAILURE, USERNAME);
 		
-		assertThat(activity.withinSuspiciousRange(oneSecondBeforeSuspicionZone), is(NORMAL));
+		assertThat(failedActivity.withinSuspiciousRange(oneSecondBeforeSuspicionZone), is(NORMAL));
 	}
 }
